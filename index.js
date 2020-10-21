0c7561bcfb37fd3a7d7c17b0dd3b1d532853eeb5
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2020-10-21 19:34:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 //All actions can be housed in the same base function.
-//Function to remove deleted items from the list
 $(function() {
-    $(".shopping-list").append(
-        //The following was copied from the HTML 
-        //to make sure all aspects of the code 
-        //were properly imported for the new item
-        //Line 10 makes sure to add the entered text 
-        //as the item name for the new <li>
-        `<li>
-        <span class="shopping-item">${item-name}</span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>`)
+    
     }
 );
-
+$(".shopping-list").append(
+    //The following was copied from the HTML 
+    //to make sure all aspects of the code 
+    //were properly imported for the new item
+    //Line 10 makes sure to add the entered text 
+    //as the item name for the new <li>
+    `<li>
+    <span class="shopping-item">${item-name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`)
 //This is the function to cross items off the list
 //Traverses the page for the correct click item
 $("body").on('click', ".shopping-item-toggle",function(){
