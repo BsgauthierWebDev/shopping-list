5f84e5e0b63ec6d3adb705e93ec45e19e57c384a
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2020-10-21 18:04:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $("body").on('click', ".shopping-item-toggle",function(){
   //console.log($(this));
-  let itemText = $(this).parents(".shopping-item")
+  let itemText = $(this).parents("li").find("shopping-item)
   console.log(itemText);
   $(itemText).toggleClass("shopping-item__checked")
   //console.log("check");
