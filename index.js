42556318e7fa79c655792dbe5356644313a2ba31
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2020-10-20 20:25:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 //Function to remove deleted items from the list
 $(function() {
     //This SHOULD delete the <li> associated with the delete button
-    $("button.shopping-item-delete").click(function() {
-        $(this).closest('li').remove();
-    })
+    // $("button.shopping-item-delete").click(function() {
+    //     $(this).closest('li').remove();
+    // })
 //})
 //Function to cross items off - Create function to change the class of the item
 //$(function() {
@@ -13,11 +13,11 @@ $(function() {
     })
 
 //Tell jQuery to add a new element for the added item
-    $('#js-shopping-list-form button') {
-        $('#shopping-list-entry').val()
-        {$('ul').append($('<div>').html(''))
-    }
-    }
+    // $('#js-shopping-list-form button') {
+    //     $('#shopping-list-entry').val()
+    //     {$('ul').append($('<div>').html(''))
+    // }
+    // }
 }
 )
 
