13be05244c15af53237113d327c671c0c3801b95
Brent Gauthier <72673424+BsgauthierWebDev@users.noreply.github.com>
2020-10-21 20:23:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,43 @@
-
+//Function to add an item to the shopping list
 function addItem(itemName){
-    //add item from input filed to list
-    
-    //add a list item for the itemName
-      $(".shopping-list").append(
-        `<li>
-            <span class="shopping-item">${itemName}</span>
-            <div class="shopping-item-controls">
-              <button class="shopping-item-toggle">
-                <span class="button-label">check</span>
-              </button>
-              <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
-              </button>
-            </div>
-          </li>`);
+    //Takes the itemName and adds it to the bottom of the list
+    $(".shopping-list").append(
+    `<li>
+        <span class="shopping-item">${itemName}</span>
+        <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+            </button>
+        </div>
+        </li>`);
     }
-    //function checkItem(){
-    //apply strikethrough to item
-    
-    //}
-    
-    //function deleteItem(){
-    //delete item from list
-    
-    //}
-    
-    $("#js-shopping-list-form").submit(function(event){
-      //when Add item is clicked get the field value and pass to addItem
-      event.preventDefault();
-      //console.log(event)
-    
-      let itemName = $(this).find("#shopping-list-entry")[0].value;
-      console.log(itemName);
-      addItem(itemName);
+        
+    //This traverses the page, looking for entered text in the box
+    //(shopping-list-form) and creates a new shopping list itemName
+    //to append using the above function
+    $("#js-shopping-list-form").submit(function(event) {
+    //This prevents the page from submitting a form to the server
+        event.preventDefault();
+    //This applies the input text to the itemName variable
+        let itemName = $(this).find(".shopping-list-entry")[0].value;
+    //This calls the itemName
+        console.log(itemName);
+    //This adds the itemName to the list
+        addItem(itemName);
     });
     
-    $("body").on('click', ".shopping-item-toggle",function(){
-      //console.log($(this));
-      let itemText = $(this).parents("li").find(".shopping-item").toggleClass("shopping-item__checked");
-      //console.log(itemText);
-      //$(itemText).toggleClass("shopping-item__checked")
-      //console.log("check");
+    //This traverses the page looking for the "shopping-item-toggle" button clicked
+    $("body").on("click", ".shopping-item-toggle", function(){
+    //This determines which list item had the button clicked and 
+    //toggles the class to "shopping-item_checked" to cross it off
+        let itemText = $(this).parents("li").find(".shopping-item").toggleClass("shopping-item_checked");
+    });
+
+    //This performs the same task as the first have of the above function
+    $("body").on("click", "shopping-item-delete", function(){
+    //But instead of changing the class, the list item is removed from the list
+        let itemText = $(this).parents("li").find(".shopping-item").remove("<li>");
     });
